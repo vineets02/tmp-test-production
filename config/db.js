acfd57099806598f1206a79e6bb5e2904b467cee
vineets02 <77182787+vineets02@users.noreply.github.com>
2025-02-18 21:55:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   const conn = await mongoose
-    .connect(mongodb+srv://vineet_02:Vineet_94@vineet.hbi5c.mongodb.net/?retryWrites=true&w=majority&appName=vineet, {
+    .connect("mongodb+srv://vineet_02:Vineet_94@vineet.hbi5c.mongodb.net/?retryWrites=true&w=majority&appName=vineet", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
